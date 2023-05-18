d0ac030f9a4131669d8c998850a47d86a40fb7c7
KITIRORI <48941917+KITIRORI@users.noreply.github.com>
2023-05-18 09:20:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,415 @@
+﻿
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+session_unset();
+?>
+ 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>School</title>
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME CSS -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+     <!-- FLEXSLIDER CSS -->
+<link href="assets/css/flexslider.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="assets/css/style.css" rel="stylesheet" />    
+  <!-- Google	Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css' />
+</head>
+<body >
+   
+ <div class="navbar navbar-inverse navbar-fixed-top " id="menu">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img class="logo-custom" src="assets/img/logo180-50.pn" alt=""  /></a>
+            </div>
+            <div class="navbar-collapse collapse move-me">
+                <ul class="nav navbar-nav navbar-right">
+                    <li ><a href="#home">HOME</a></li>
+                     <li><a href="#faculty-sec">OUR STAFF</a></li>
+                    <li><a href="#About Us">ABOUT US</a></li>
+                     <li><a href="#course-sec">STUDENTS</a></li>
+                     <li><a href="#contact-sec">CONTACT</a></li>
+					      <li><a href="portal">LOGIN</a></li>
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+      <!--NAVBAR SECTION END-->
+       <div class="home-sec" id="home" >
+           <div class="overlay">
+ <div class="container">
+           <div class="row text-center " >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+                <div class="flexslider set-flexi" id="main-section" >
+                    <ul class="slides move-me">
+                        <!-- Slider 01 -->
+                        <li>
+                              <h3>Delivering Quality Education</h3>
+                           <h1>THE UNIQUE METHOD</h1>
+                             <a  href="#features" class="btn btn-primary btn-lg" >
+                              LOGIN
+                            </a>
+                             <a  href="#faculty-sec" class="btn btn-danger btn-lg" >
+                                OUR STAFFS
+                            </a>
+                        </li>
+                        <!-- End Slider 01 -->
+                        
+                        <!-- Slider 02 -->
+                        <li>
+                            <h3>Delivering Quality Education</h3>
+                           <h1>UNMATCHED APPROACH</h1>
+                             <a  href="#features" class="btn btn-primary btn-lg" >
+                              LOGIN
+                            </a>
+                             <a  href="#faculty-sec" class="btn btn-danger btn-lg" >
+                                OUR STAFFS
+                            </a>
+                        </li>
+                        <!-- End Slider 02 -->
+                        
+                        <!-- Slider 03 -->
+                        <li>
+                            <h3>Delivering Quality Education</h3>
+                           <h1>WE BELIEVE EVERY THING IS POSSIBLE</h1>
+                              <a  href="#features" class="btn btn-primary btn-lg" >
+                              LOGIN
+                            </a>
+                             <a  href="#faculty-sec" class="btn btn-danger btn-lg" >
+                                OUR STAFFS
+                            </a>
+                        </li>
+                        <!-- End Slider 03 -->
+                    </ul>
+                </div>
+                   
+     
+              
+              
+            </div>
+                
+               </div>
+                </div>
+           </div>
+           
+       </div>
+       <!--HOME SECTION END-->   
+    <div  class="tag-line" >
+         <div class="container">
+           <div class="row  text-center" >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+        <h2 data-scroll-reveal="enter from the bottom after 0.1s" ><i class="fa fa-circle-o-notch"></i> WELCOME TO THE EMAN GIRLS SEC SCHOOL <i class="fa fa-circle-o-notch"></i> </h2>
+                   </div>
+               </div>
+             </div>
+        
+    </div>
+    <!--HOME SECTION TAG LINE END-->   
+         <div id="features-sec" class="container set-pad" >
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.2s"  class="header-line">MISSION VISION & CORE VALUES </h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s" >
+                    
+                         </p>
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+
+           <div class="row" >
+           
+               
+                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="about-div">
+                     <i class="fa fa-paper-plane-o fa-4x icon-round-border" ></i>
+                   <h3 >MISSION</h3>
+               
+                   <p >
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                        Aenean commodo . 
+                       
+                   </p>
+               <a href="#" class="btn btn-info btn-set"  >Read More</a>
+                </div>
+                   </div>
+                   <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.5s">
+                     <div class="about-div">
+                     <i class="fa fa-bolt fa-4x icon-round-border" ></i>
+                   <h3 >VISION</h3>
+                
+                     
+                   <p >
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                        Aenean commodo . 
+                       
+                   </p>
+               <a href="#" class="btn btn-info btn-set"  >Read More</a>
+                </div>
+                   </div>
+                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.6s">
+                     <div class="about-div">
+                     <i class="fa fa-magic fa-4x icon-round-border" ></i>
+                   <h3 >CORE VALUES</h3>
+               
+                   <p >
+                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                        Aenean commodo . 
+                       
+                   </p>
+               <a href="#" class="btn btn-info btn-set"  >Read More</a>
+                </div>
+                   </div>
+                 
+                 
+               </div>
+             </div>
+   <!-- FEATURES SECTION END-->
+    <div id="faculty-sec" >
+    <div class="container set-pad">
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">OUR STAFF </h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s">
+                    
+                         </p>
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+           <div class="row" >
+           
+               
+                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="faculty-div">
+                     <img src="assets/img/faculty/1.jpg"  class="img-rounded" />
+                   <h3 >ROXI CHI LUENA </h3>
+                 <hr />
+                         <h4>PRINCIPAL<br /> </h4>
+                   <p >
+                       
+                   </p>
+                </div>
+                   </div>
+                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.5s">
+                     <div class="faculty-div">
+                     <img src="assets/img/faculty/2.jpg"  class="img-rounded" />
+                   <h3 >JANE DEO ALEX</h3>
+                 <hr />
+                         <h4>DEPUTY PRINCIPAL <br /></h4>
+                   <p >
+                      
+                       
+                   </p>
+                </div>
+                   </div>
+               <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.6s">
+                     <div class="faculty-div">
+                     <img src="assets/img/faculty/3.jpg" class="img-rounded" />
+                   <h3 >RUBY DECORSA</h3>
+                 <hr />
+                         <h4>DEAN OF STUDENT <br /></h4>
+                   <p >
+                      
+                       
+                   </p>
+                </div>
+                   </div>
+                 
+               </div>
+             </div>
+        </div>
+    <!-- FACULTY SECTION END-->
+      <div id="course-sec" class="container set-pad">
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">OUR STUDENTS </h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s">
+                       </p>
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+
+           <div class="row set-row-pad" >
+           <div class="col-lg-12 col-md-12 col-sm-12 " data-scroll-reveal="enter from the bottom after 0.4s" >
+                 <img src="assets/img/building.jpg" class="img-thumbnail" />
+           </div>
+            
+             
+                 
+                 
+               </div>
+             </div>
+      <!-- COURSES SECTION END-->
+	    <div id="About Us" >
+    <div class="container set-pad">
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">About Us </h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s">
+                    
+                         </p>
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+           <div class="row" >
+           
+               
+                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="faculty-div">
+                     <img src="assets/img/faculty/1.jpg"  class="img-rounded" />
+                   <h3 >ROXI CHI LUENA </h3>
+                 <hr />
+                         <h4>PRINCIPAL<br /> </h4>
+                   <p >
+                       
+                   </p>
+                </div>
+                   </div>
+                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.5s">
+                     <div class="faculty-div">
+                     <img src="assets/img/faculty/2.jpg"  class="img-rounded" />
+                   <h3 >JANE DEO ALEX</h3>
+                 <hr />
+                         <h4>DEPUTY PRINCIPAL <br /></h4>
+                   <p >
+                      
+                       
+                   </p>
+                </div>
+                   </div>
+               <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.6s">
+                     <div class="faculty-div">
+                     <img src="assets/img/faculty/3.jpg" class="img-rounded" />
+                   <h3 >RUBY DECORSA</h3>
+                 <hr />
+                         <h4>DEAN OF STUDENT <br /></h4>
+                   <p >
+                      
+                       
+                   </p>
+                </div>
+                   </div>
+                 
+               </div>
+             </div>
+        </div>
+	  
+	  
+    <div id="contact-sec"   >
+           <div class="overlay">
+ <div class="container set-pad">
+      <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line" >CONTACT US  </h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s">
+                     
+                         </p>
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+           <div class="row set-row-pad"  data-scroll-reveal="enter from the bottom after 0.5s" >
+           
+               
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control "  required="required" placeholder="Your Name" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control " required="required"  placeholder="Your Email" />
+                        </div>
+                        <div class="form-group">
+                            <textarea name="message" required="required" class="form-control" style="min-height: 150px;" placeholder="Message"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-info btn-block btn-lg">SUBMIT REQUEST</button>
+                        </div>
+
+                    </form>
+                </div>
+
+                   
+     
+              
+              
+                
+               </div>
+                </div>
+          </div> 
+       </div>
+     <div class="container">
+             <div class="row set-row-pad"  >
+    <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1 " data-scroll-reveal="enter from the bottom after 0.4s">
+
+                    <h2 ><strong>Our Location </strong></h2>
+        <hr />
+                    <div>
+                        <h4>565556, Tudor,</h4>
+                        <h4>Mombasa.</h4>
+                        <h4><strong>Call:</strong>  +071234444 </h4>
+                        <h4><strong>Email: </strong>info@emangirls.com</h4>
+                    </div>
+
+
+                </div>
+                 <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1" data-scroll-reveal="enter from the bottom after 0.4s">
+
+                    <h2 ><strong>Social Conectivity </strong></h2>
+        <hr />
+                    <div >
+                        <a href="#">  <img src="assets/img/Social/facebook.png" alt="" /> </a>
+                     <a href="#"> <img src="assets/img/Social/google-plus.png" alt="" /></a>
+                     <a href="#"> <img src="assets/img/Social/twitter.png" alt="" /></a>
+                    </div>
+                    </div>
+
+
+                </div>
+                 </div>
+          
+     <!-- CONTACT SECTION END-->
+    <div id="footer">
+          &copy 2017 emangirls.com | All Rights Reserved |  <a href="http://binarytheme.com" style="color: #fff" target="_blank">Design by : skywebtechnologies.com</a>
+    </div>
+     <!-- FOOTER SECTION END-->
+   
+    <!--  Jquery Core Script -->
+    <script src="assets/js/jquery.js"></script>
+    <!--  Core Bootstrap Script -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!--  Flexslider Scripts --> 
+         <script src="assets/js/jquery.flexslider.js"></script>
+     <!--  Scrolling Reveal Script -->
+    <script src="assets/js/scrollReveal.js"></script>
+    <!--  Scroll Scripts --> 
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!--  Custom Scripts --> 
+         <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
